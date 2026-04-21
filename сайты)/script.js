b32edbfdfb34de14569fad6b87b7fe59f927a29b
ArtyrEsin <artyresen07@gmail.com>
2026-04-22 00:59:01 +0500

    Add files via upload

--- сайты)/script.js
+++ сайты)/script.js
@@ -1,10 +1,43 @@
 const burger = document.getElementById("burger");
-const menu = document.querySelector(".menu");
+const menu = document.getElementById("menu");
+const searchInput = document.getElementById("searchInput");
+const productsList = document.getElementById("productsList");
+const productCards = document.querySelectorAll(".product-card");
+const emptyText = document.getElementById("emptyText");
+const buyButtons = document.querySelectorAll(".buy-btn");
 const contactForm = document.getElementById("contactForm");
 const formMessage = document.getElementById("formMessage");
 
 burger.addEventListener("click", function () {
   menu.classList.toggle("show");
+});
+
+searchInput.addEventListener("input", function () {
+  const value = searchInput.value.toLowerCase().trim();
+  let visibleCount = 0;
+
+  productCards.forEach(function (card) {
+    const name = card.dataset.name.toLowerCase();
+
+    if (name.includes(value)) {
+      card.style.display = "block";
+      visibleCount++;
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  if (visibleCount === 0) {
+    emptyText.textContent = "Товары не найдены.";
+  } else {
+    emptyText.textContent = "";
+  }
+});
+
+buyButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    alert("Товар добавлен в корзину.");
+  });
 });
 
 contactForm.addEventListener("submit", function (e) {
